92bc415fce684c068a981cd601c41867051c4711
FZRaizy <fahdinzikri8@gmail.com>
2023-01-26 15:00:00 +0700

    Add end2end test

---
+++ e2e/Liking_Spot.spec.js
@@ -0,0 +1,24 @@
+const assert = require('assert')
+
+/* eslint-disable */
+Feature('restaurant')
+
+Scenario('Liking a spot', async ({ I }) => {
+    I.amOnPage('/')
+    I.waitForElement('.gridItem', 10)
+    I.seeElement('.gridItem')
+    const firstRestoName = await I.grabTextForm(locate('.gridItem').first())
+    I.click(locate('.gridItem').first())
+
+    I.waitForElement('#likeButton', 10)
+    I.seeElement('#likeButton')
+    I.click('#likeButton')
+
+    I.amOnPage('#/favorites')
+    I.waitForElement('.gridList', 10)
+    I.seeElement('.gridList')
+    const likedRestoName = await I.grabTextForm(locate('.gridItem').first())
+    
+    assert.strictEqual(firstRestoName, likedRestoName)
+    
+})
---
+++ e2e/Unliking_Spot.spec.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+Feature('restaurant')
+
+Scenario('Unliking a resto', ({I}) => {
+    I.amOnPage('/#/detail/rqdv5juczeskfw1e867')
+    I.waitForElement('#likeButton', 10)
+    I.seeElement('#likeButton')
+    I.click('#likeButton')
+
+    I.amOnPage('/#/favorites')
+    I.waitForElement('.gridList', 10)
+    I.seeElement('.gridList')
+    I.seeElement('.gridItem')
+    I.click('.gridItem')
+
+    I.waitForElement('#likeButton', 10)
+    I.seeElement('#likeButton')
+    I.click('#likeButton')
+
+    I.amOnPage('/#/favorites')
+    I.seeElement('.noData')
+})  
